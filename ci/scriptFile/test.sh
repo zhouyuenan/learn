0d5d41006a1990cdcba12fb7718fc1cd2e3d79b3
周月南 <2091199055@qq.com>
2022-11-18 22:12:32 +0800

    test

---
+++ ci/scriptFile/test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# ------------------------------------------------
+# Program:
+#       This program is used to be tested.
+#       此项目用于测试使用
+# History:
+#       2022/11/18 zhouyuenan First release
+# ------------------------------------------------
+GIT_URL=
