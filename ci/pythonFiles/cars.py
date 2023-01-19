4f6eae017c51f3af03516bbd9073976e671d97b6
周月南 <2091199055@qq.com>
2023-01-19 15:09:07 +0800

    【修改时间】：2023/01/19 【修改人】：周月南 【审核人】：周月南 【提交信息】：新增python文件夹，重构代码仓路径

---
+++ ci/pythonFiles/cars.py
@@ -0,0 +1,13 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+cars.sort(reverse=True)
+print(cars)
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list again:")
+print(cars)
